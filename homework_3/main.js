d8daffee12cde83fc2807ae7d55f36ee2e38ffba
buzzsquez <buzzsquez@gmail.com>
2022-08-18 16:55:38 +0300

    homework_3_update

--- homework_3/main.js
+++ homework_3/main.js
@@ -7,17 +7,23 @@ Array.prototype.myFilter = function (callbackFn, thisArg) {
   const result = [];
 
   for (let i = 0; i < this.length; i++) {
-    if (i in this) {
       const current = callbackFn.call(context, this[i], i, this);
 
       if (current) {
         result.push(this[i]);
       }
-    }
   }
 
   return result;
 };
+
+const arr = [1, 2, 3, , 5, 6, 7, , ,10];
+
+function isBigEnough(value) {
+  return value >= 3;
+}
+const result = arr.myFilter(isBigEnough)
+console.log(result)
 
 //----------------------------------------------------------------------------------
 
